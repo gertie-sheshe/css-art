272307a4739f3c0413b50b5e8e9bd3fa676259bf
gertie-sheshe <gertrude.nyenyeshi@gmail.com>
2020-08-14 22:52:03 +0300

    add mouth

--- src/components/cool-face/cool-face-component.js
+++ src/components/cool-face/cool-face-component.js
@@ -4,17 +4,18 @@ import './cool-face.scss';
 const CoolFace = () => (
     <div className="container-one">
         <div className="container">
-          <div className="head">
+            <div className="head">
                 <div className="eye left">
                     <div className="cutout"></div>
                 </div>
                 <div className="eye right">
                     <div className="cutout"></div>
                 </div>
-                <div className="mouth">
+                <div className="mouth"> </div>
+            </div>
 
-                </div>
-          </div>
+            <div className="body"></div>
+            
         </div>
     </div>
 );
