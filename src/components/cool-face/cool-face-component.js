117435b61ba4463428e42d9b74a0a6e182bceb03
gertie-sheshe <gertrude.nyenyeshi@gmail.com>
2020-08-14 20:49:37 +0300

    initial work on cool face

---
+++ src/components/cool-face/cool-face-component.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './cool-face.scss';
+
+const CoolFace = () => (
+    <div className="container-one">
+        <div className="container">
+          <div className="head">
+            
+          </div>
+        </div>
+    </div>
+);
+
+export default CoolFace;
