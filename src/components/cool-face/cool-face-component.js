1f168b87f84003f44e03d1ea3e3ef7a939890a98
gertie-sheshe <gertrude.nyenyeshi@gmail.com>
2020-08-14 21:35:45 +0300

    add eyes

--- src/components/cool-face/cool-face-component.js
+++ src/components/cool-face/cool-face-component.js
@@ -5,7 +5,15 @@ const CoolFace = () => (
     <div className="container-one">
         <div className="container">
           <div className="head">
-            
+                <div className="eye left">
+                    <div className="cutout"></div>
+                </div>
+                <div className="eye right">
+                    <div className="cutout"></div>
+                </div>
+                <div className="mouth">
+
+                </div>
           </div>
         </div>
     </div>
